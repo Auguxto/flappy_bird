8c2770a4839ffb97ad83c9719f65f2042e3719a1
Victor Augusto Ferreira <victoraugusferreira@gmail.com>
2024-09-04 16:13:50 -0300

    chore: refactor camera, split code

--- src/core/camera/mod.rs
+++ src/core/camera/mod.rs
@@ -1,13 +1,11 @@
 use bevy::prelude::*;
 
+mod camera_systems;
+
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, setup_camera);
+        app.add_systems(Startup, camera_systems::setup_camera);
     }
-}
-
-fn setup_camera(mut commands: Commands) {
-    commands.spawn(Camera2dBundle::default());
 }
